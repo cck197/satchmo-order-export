36875cfb9e887d1350edef80e740a521dd24ce0b
Christopher Kelly <cck197@cck197.net>
2014-11-20 09:34:04 -0800

    Add export all orders.

--- order_export/urls.py
+++ order_export/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('order_export.views',
     (r'^new_orders.csv$', 'admin_order_export', {'status':'New'}, 'admin_new_order_export'),
+    (r'^all_orders.csv$', 'admin_order_export', {}, 'admin_all_order_export'),
 )
